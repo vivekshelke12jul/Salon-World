704630653e3400f07b482bc221f467dd7bfbecd7
Vivek Shelke <vivekshelke12jul@gmail.com>
2025-05-08 23:10:17 +0530

    first commit

---
+++ userMicroservice/src/test/java/com/vivek/userMicroservice/UserMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vivek.userMicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
