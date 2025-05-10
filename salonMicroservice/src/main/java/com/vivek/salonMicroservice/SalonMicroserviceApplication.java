11de72edd7a6ddf72486b3fa3b3ac6b806434510
Vivek Shelke <vivekshelke12jul@gmail.com>
2025-05-10 22:52:38 +0530

    salon microservice first commit

---
+++ salonMicroservice/src/main/java/com/vivek/salonMicroservice/SalonMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.vivek.salonMicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalonMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalonMicroserviceApplication.class, args);
+	}
+
+}
---
+++ salonMicroservice/src/test/java/com/vivek/salonMicroservice/SalonMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vivek.salonMicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalonMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
