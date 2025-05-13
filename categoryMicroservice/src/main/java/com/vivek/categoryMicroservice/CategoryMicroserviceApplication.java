3963e37ae14388f8998b03ed720dcc07cea7efb4
Vivek Shelke <vivekshelke12jul@gmail.com>
2025-05-13 08:10:07 +0530

    category microservice first commit

---
+++ categoryMicroservice/src/main/java/com/vivek/categoryMicroservice/CategoryMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.vivek.categoryMicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CategoryMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CategoryMicroserviceApplication.class, args);
+	}
+
+}
---
+++ categoryMicroservice/src/test/java/com/vivek/categoryMicroservice/CategoryMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vivek.categoryMicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CategoryMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
