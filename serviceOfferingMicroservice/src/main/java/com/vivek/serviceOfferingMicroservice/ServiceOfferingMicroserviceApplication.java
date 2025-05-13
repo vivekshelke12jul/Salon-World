c90856a79caa6e98f2b29999d6d23b3bd5ffbf02
Vivek Shelke <vivekshelke12jul@gmail.com>
2025-05-13 09:02:34 +0530

    service offering microservice first commit

---
+++ serviceOfferingMicroservice/src/main/java/com/vivek/serviceOfferingMicroservice/ServiceOfferingMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.vivek.serviceOfferingMicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceOfferingMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceOfferingMicroserviceApplication.class, args);
+	}
+
+}
---
+++ serviceOfferingMicroservice/src/test/java/com/vivek/serviceOfferingMicroservice/ServiceOfferingMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vivek.serviceOfferingMicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceOfferingMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
