cc49301be0e8a24263c0c50bd65f17059710fc37
Vivek Shelke <vivekshelke12jul@gmail.com>
2025-05-15 09:26:01 +0530

    payment microservice first commit

---
+++ paymentMicroservice/src/test/java/com/vivek/paymentMicroservice/PaymentMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vivek.paymentMicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
